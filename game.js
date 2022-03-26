a82ce315d267917a93b282866e5f7149dc645e75
bujarr <bujarskola@gmail.com>
2022-03-26 20:10:06 +0100

    added gamOverScreen function + more logic

--- game.js
+++ game.js
@@ -10,8 +10,21 @@ const grid = 10; // 30 grids per row
 const x = new Array(900);
 const y = new Array(900);
 
+for (let z = 0; z <= size; z++) {
+    x[z] = 40 - z * 10;
+    y[z] = 40;
+}
+
 // All functions that will continuously be run with a timeout of 100ms.
 function cyclic() {
+    
 
     setTimeout("cyclic()", 100);
+}
+
+function gameOverScreen() {
+    context.fillStyle = 'white';
+    context.textBaseline = 'middle';
+    context.textAlign = 'center';
+    context.fillText('GAME OVER!', canvas.width / 2, canvas.height / 2);
 }
