bd1b8f261a006c72af3cf378df12ff206f704c35
bujarr <bujarskola@gmail.com>
2022-03-26 21:06:48 +0100

    added display method + food variables

--- game.js
+++ game.js
@@ -5,6 +5,8 @@ const context = canvas.getContext('2d');
 let gameIsOver = false;
 let size = 2; // Default body size of snake is 2
 const grid = 10; // 30 grids per row
+let eatenFood = true;
+let foodX, foodY;
 
 // 900 because 30x30 == 900
 const x = new Array(900);
@@ -18,13 +20,46 @@ for (let z = 0; z <= size; z++) {
 // All functions that will continuously be run with a timeout of 100ms.
 function cyclic() {
     
-
+    display();
     setTimeout("cyclic()", 100);
 }
+
+cyclic();
+
 
 function gameOverScreen() {
     context.fillStyle = 'white';
     context.textBaseline = 'middle';
     context.textAlign = 'center';
     context.fillText('GAME OVER!', canvas.width / 2, canvas.height / 2);
+}
+
+function display() {
+    context.clearRect(0, 0, canvas.width, canvas.height);
+    if (gameIsOver) {
+        gameOverScreen();
+
+    } else {
+        if (eatenFood) {
+            putFood();
+            eatenFood = false;
+
+        }
+
+        // Colour the food red
+        context.beginPath();
+        context.fillStyle = "Red";
+        context.arc(foodX, foodY, grid / 2, 0, 2 * Math.PI, false);
+        context.fill();
+        context.closePath();
+
+        // Colour the entire snakes body green
+        for (let z = 0; z < size; z++) {
+            context.beginPath();
+            context.fillStyle = "Green";
+            context.arc(x[z], y[z], grid / 2, 0, 2 * Math.PI, false);
+            context.fill();
+            context.closePath();
+        }
+    }
 }
